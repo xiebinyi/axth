3fef1a42c6cd86435c3419dfdae2d138c5b0f32a
xiebinyi <xiebinyi@gmail.com>
2015-03-31 15:50:54 +0800

    Initial Commit

---
+++ axth/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  axth
+//
+//  Created by xiebinyi on 15/3/31.
+//  Copyright (c) 2015年 bjbl. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ axth/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  axth
+//
+//  Created by xiebinyi on 15/3/31.
+//  Copyright (c) 2015年 bjbl. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
